21aaff8590e9c2d510a9102854cd0a376242d9fa
Jesper Asplund <jesper.asplund@elev.ga.ntig.se>
2022-09-08 15:44:46 +0200

    Interaktiv berättelse

--- Interaktiv_berättelse/Program.cs
+++ Interaktiv_berättelse/Program.cs
@@ -47,12 +47,16 @@ Console.WriteLine($"{karaktär} får panick och börjar leta efter lamp knappen,
 Console.ReadLine();
 Console.WriteLine($"{karaktär} har dött");
 Console.ReadLine();
+Console.WriteLine("Du har förlorat");
+Console.ReadLine();
 }
 else if ( yes2 == "b")
 {
 Console.WriteLine($"{karaktär} blir obekväm och flyr upp för trappan, {karaktär} hör steg bakom sig men hinner ut ur huset precis i tid");
 Console.ReadLine();
 Console.WriteLine($"{karaktär} har flytt huset och överlevt");
+Console.ReadLine();
+Console.WriteLine("Du har vunnit");
 Console.ReadLine();
 }
 }
@@ -65,21 +69,25 @@ if ( yes == "b")
 Console.WriteLine($"{karaktär} väljer att gå ut i köket där han hittar en kniv");
 Console.ReadLine();
 Console.WriteLine($"Vad ska {karaktär} göra?");
-Console.WriteLine("a = leta efter lamp knappen");
-Console.WriteLine("b = fly tillbaka upp för trappan");
+Console.WriteLine("a = ta upp kniven för att försvara sig själv");
+Console.WriteLine("b = låt kniven vara så man inte skär sig");
 string yes2 = Console.ReadLine();
 if ( yes2 == "a")
 {
-Console.WriteLine($"{karaktär} får panick och börjar leta efter lamp knappen, ett morrande hörs bakifrån");
+Console.WriteLine($"{karaktär} tar upp kniven, ur källaren kommer ett monster springande, {karaktär} hugger monstret med kniven ");
 Console.ReadLine();
-Console.WriteLine($"{karaktär} har dött");
+Console.WriteLine($"{karaktär} har överlevt");
+Console.ReadLine();
+Console.WriteLine("Du har vunnit, grattis");
 Console.ReadLine();
 }
 else if ( yes2 == "b")
 {
-Console.WriteLine($"{karaktär} blir obekväm och flyr upp för trappan, {karaktär} hör steg bakom sig men hinner ut ur huset precis i tid");
+Console.WriteLine($"{karaktär} väljer att lämna kniven, fotsteg hörs från källaren och ett monster attackerar {karaktär} ");
 Console.ReadLine();
-Console.WriteLine($"{karaktär} har flytt huset och överlevt");
+Console.WriteLine($"{karaktär} hade inget att försvara sig med, {karaktär} har dött");
+Console.ReadLine();
+Console.WriteLine("Du har förlorat");
 Console.ReadLine();
 }
 }
